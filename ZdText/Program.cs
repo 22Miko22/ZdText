3c8f93024310f0137e98568e262aca186399d327
Daniil Pushkarov <dakapushks13@gmail.com>
2025-12-14 15:39:06 +0100

    Добавьте файлы проекта.

---
+++ ZdText/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int liczbaLiterek; 
+//Console.WriteLine("Wybierz rodzaj literek.");
+Console.WriteLine("Podaj słowo");
+string slowo = Console.ReadLine();
+slowo = slowo.ToUpper();
+liczbaLiterek = slowo.Length;
+Console.Write("Twoje słowo to: ");
+Console.WriteLine(slowo);
+Console.Write("Liczba literek w Twoim słowie to: ");
+Console.WriteLine(liczbaLiterek);
+
+
